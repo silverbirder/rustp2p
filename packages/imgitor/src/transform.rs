b34288c2c5967e92c5c70918587aebbc428d97a8
shibamoto <silverbirder@gmail.com>
2022-02-05 23:03:09 +0900

    Fmm learn image crate

--- packages/imgitor/src/transform.rs
+++ packages/imgitor/src/transform.rs
@@ -1,21 +1,42 @@
+use image::GenericImageView;
 use walkdir::WalkDir;
-use image::{GenericImageView};
-use image::io::Reader as ImageReader;
+use regex::Regex;
 
 pub fn rename() {
-    // let wark = WalkDir::new("./lake/a").sort_by_file_name();
-    // for a in wark {
-    //     let dir = a.unwrap();
-    //     println!("{:?}", dir.file_name());
-    //     println!("{:?}", dir.file_type().is_dir());
-    //     println!("{:?}", dir.file_type().is_file());
-    //     println!("{:?}", dir.path());
-    // }
-    let img = ImageReader::open("./lake/a/0001.png").unwrap().decode().unwrap();
-    // let img = image::open("./lake/a/0001.png").unwrap();
-    println!("dimensions {:?}", img.dimensions());
-    println!("{:?}", img.color());
-    img.save("./lake/a/0001.jpeg").unwrap();
+    let wark = WalkDir::new("./lake/a").sort_by_file_name();
+    let re = Regex::new(r"/[^.]+\.(jpg|png)$").unwrap();
+    let mut i = 0;
+    for a in wark {
+        let dir = a.unwrap();
+        if dir.file_type().is_dir() {
+            continue;
+        }
+        let result = image::open(dir.path());
+        if result.is_err() {
+            continue;
+        } 
+        let mut img = result.unwrap();
+        let s = format!("{:08}", i);
+        let name = String::from("/") + s.as_str() + &String::from(".jpeg");
+        let path = re.replace_all(dir.path().to_str().unwrap(), name).to_string();
+        println!("{:?}", path);
+        if img.width() > img.height() {
+            println!(">");
+        };
+        // while img.width() * img.height() > 500 * 1000 {
+        //     println!("{:?}", img.dimensions());
+        //     let w = img.width() as f64;
+        //     let h = img.height() as f64;
+        //     img = img.resize(
+        //         ((w * 0.9).round() as i64).try_into().unwrap(),
+        //         ((h * 0.9).round() as i64).try_into().unwrap(),
+        //         image::imageops::FilterType::CatmullRom,
+        //     );
+        // }
+        i = i + 1;
+        // img.save(path).unwrap();
+    }
+    // img.crop(0, 0, 764, 600)
 }
 
 pub fn reimage() {
