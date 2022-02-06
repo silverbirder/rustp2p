b6f76fc26e0a2be48113495bd10f8f44e91e3b08
shibamoto <silverbirder@gmail.com>
2022-02-06 11:35:03 +0900

    Dirty code, but the code that splits the landscape image worked

--- packages/imgitor/src/transform.rs
+++ packages/imgitor/src/transform.rs
@@ -4,8 +4,7 @@ use regex::Regex;
 
 pub fn rename() {
     let wark = WalkDir::new("./lake/a").sort_by_file_name();
-    let re = Regex::new(r"/[^.]+\.(jpg|png)$").unwrap();
-    let mut i = 0;
+    let mut i = 1;
     for a in wark {
         let dir = a.unwrap();
         if dir.file_type().is_dir() {
@@ -14,29 +13,57 @@ pub fn rename() {
         let result = image::open(dir.path());
         if result.is_err() {
             continue;
-        } 
+        }
         let mut img = result.unwrap();
         let s = format!("{:08}", i);
-        let name = String::from("/") + s.as_str() + &String::from(".jpeg");
-        let path = re.replace_all(dir.path().to_str().unwrap(), name).to_string();
-        println!("{:?}", path);
+        let dir_path = dir.path().to_str().unwrap().replace(dir.file_name().to_str().unwrap(), "");
+        let dist_path = dir_path + &s + ".jpeg";
         if img.width() > img.height() {
-            println!(">");
-        };
-        // while img.width() * img.height() > 500 * 1000 {
-        //     println!("{:?}", img.dimensions());
-        //     let w = img.width() as f64;
-        //     let h = img.height() as f64;
-        //     img = img.resize(
-        //         ((w * 0.9).round() as i64).try_into().unwrap(),
-        //         ((h * 0.9).round() as i64).try_into().unwrap(),
-        //         image::imageops::FilterType::CatmullRom,
-        //     );
-        // }
-        i = i + 1;
-        // img.save(path).unwrap();
+            let mut left_img = img.crop(0, 0, img.width() / 2, img.height());
+            let mut right_img = img.crop(img.width() / 2, 0, img.width() / 2, img.height());
+            while left_img.width() * left_img.height() > 500 * 1000 {
+                println!("{:?}", left_img.dimensions());
+                let w = left_img.width() as f64;
+                let h = left_img.height() as f64;
+                left_img = left_img.resize(
+                    ((w * 0.9).round() as i64).try_into().unwrap(),
+                    ((h * 0.9).round() as i64).try_into().unwrap(),
+                    image::imageops::FilterType::CatmullRom,
+                );
+            }
+            left_img.save(dist_path).unwrap();
+            i = i + 1;
+
+            while right_img.width() * right_img.height() > 500 * 1000 {
+                println!("{:?}", right_img.dimensions());
+                let w = right_img.width() as f64;
+                let h = right_img.height() as f64;
+                right_img = right_img.resize(
+                    ((w * 0.9).round() as i64).try_into().unwrap(),
+                    ((h * 0.9).round() as i64).try_into().unwrap(),
+                    image::imageops::FilterType::CatmullRom,
+                );
+            }
+            let s = format!("{:08}", i);
+            let dir_path = dir.path().to_str().unwrap().replace(dir.file_name().to_str().unwrap(), "");
+            let dist_path = dir_path + &s + ".jpeg";
+            right_img.save(dist_path).unwrap();
+            i = i + 1;
+        } else {
+            while img.width() * img.height() > 500 * 1000 {
+                println!("{:?}", img.dimensions());
+                let w = img.width() as f64;
+                let h = img.height() as f64;
+                img = img.resize(
+                    ((w * 0.9).round() as i64).try_into().unwrap(),
+                    ((h * 0.9).round() as i64).try_into().unwrap(),
+                    image::imageops::FilterType::CatmullRom,
+                );
+            }
+            i = i + 1;
+            img.save(dist_path).unwrap();
+        }
     }
-    // img.crop(0, 0, 764, 600)
 }
 
 pub fn reimage() {
