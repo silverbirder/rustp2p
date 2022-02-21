93b5ebd043724c35ff5eade5b17361fd0bdb5ba4
shibamoto <silverbirder@gmail.com>
2022-02-21 22:26:30 +0900

    Add compress test code

--- packages/imgitor/src/czip.rs
+++ packages/imgitor/src/czip.rs
@@ -145,3 +145,13 @@ pub fn extract(f: &str, pb: &path::PathBuf) -> path::PathBuf {
     }
     return extracted_folder_path.clone();
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_compress() {
+        assert_eq!(1, 1);
+    }
+}
