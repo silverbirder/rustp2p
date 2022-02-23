760a9d3a8a95abc9e01b7dd9a8445c54ccf4f178
shibamoto <silverbirder@gmail.com>
2022-02-23 18:16:30 +0900

    Add comment

--- packages/imgitor/src/crar.rs
+++ packages/imgitor/src/crar.rs
@@ -11,3 +11,18 @@ pub fn extract(f: &str, p: &path::PathBuf) -> path::PathBuf {
 
     return p.to_path_buf();
 }
+/*
+もともと、以下のcrateを使っていた。
+https://crates.io/crates/unrar
+日本語の名前がファイルに含まれていると、Dockerのdebian内でrarファイルを展開できなかった。
+
+例
+* a.rar
+  * ほげ.txt
+
+rarを展開できるクレートを探したが、特に他に使えそうなものがなかった。
+https://crates.io/search?q=rar
+
+仕方がないが、unrarを外部コマンドとして実行する方針で実装した。
+外部コマンドは、7zを使う。
+*/
